339196a650b2e51f8dffa7ee075bfbf476278597
Sumer Singh <singhsumersingh35@gmail.com>
2026-06-24 14:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (82.07%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -2,26 +2,48 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         // sumer singh
-        
-        
-        int sorted=false;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            vector<int> b;
-            for(int j=0;j<n;j++){
 
-            b.push_back(nums[(j+i)%nums.size()]);
+
+        int count =0;
+        int n=nums.size();
+        if(nums[n-1]>nums[0]){
+                count++;
             }
-            if(is_sorted(b.begin(),b.end())){
-                sorted = true;
-                break;
+
+        for(int i = 1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+                count++;
             }
+            
+
+        }
+        if(count<=1){
+            return true;
+        }
+        else{
+            return false;
         }
 
+        
+        
+        // int sorted=false;
+        // int n = nums.size();
+        // for(int i=0;i<n;i++){
+        //     vector<int> b;
+        //     for(int j=0;j<n;j++){
+
+        //     b.push_back(nums[(j+i)%nums.size()]);
+        //     }
+        //     if(is_sorted(b.begin(),b.end())){
+        //         sorted = true;
+        //         break;
+        //     }
+        // }
+
 
         
 
-    return sorted;
+    // return sorted;
         
     }
 };
