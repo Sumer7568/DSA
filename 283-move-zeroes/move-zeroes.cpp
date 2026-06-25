8e31d4d1f0eef52a5dfdcafffff7ae189a8cef98
Sumer Singh <singhsumersingh35@gmail.com>
2026-06-25 15:37:31 +0530

    Time: 3 ms (14.71%) | Memory: 24 MB (18.92%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,23 +1,42 @@
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         //sumer singh
+
+//         int left =0;
+//         int right =1;
+
+//         for(;right<nums.size();right++){
+//             if(nums[left]==0 && nums[right]!=0){
+//                 swap(nums[left],nums[right]);
+//                 left++;
+//             }
+//             else if (nums[left]!=0){
+//                 left++;
+//             }
+
+//         }
+        
+
+        
+//     }
+// };
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        //sumer singh
+        // sumer singh
+        int left = 0;  // Writer
 
-        int left =0;
-        int right =1;
-
-        for(;right<nums.size();right++){
-            if(nums[left]==0 && nums[right]!=0){
-                swap(nums[left],nums[right]);
+        // 'right' is the Explorer
+        for(int right = 0; right < nums.size(); right++) {
+            
+            // The moment we find ANY non-zero number...
+            if(nums[right] != 0) {
+                // Throw it into the left-most available slot
+                swap(nums[left], nums[right]);
                 left++;
             }
-            else if (nums[left]!=0){
-                left++;
-            }
-
         }
-        
-
-        
     }
 };
