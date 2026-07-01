233d9bdaacd50d07c680d122fb28b8cc1c8cda5e
Sumer Singh <singhsumersingh35@gmail.com>
2026-07-01 18:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (24.83%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     int maxProfit(vector<int>& p) {
-        int left =0;
-        int right=1;
+        // sumer singh
         int n=p.size();
         int maxi=0;
-        for(;right<n;right++){
-            if(p[right]>p[left]){
-                maxi=max(maxi,p[right]-p[left]);
-
+        int min = p[0];
+    
+        for(int i=1;i<n;i++){
+            if(p[i]>min){
+                maxi=max(maxi,p[i]-min);
             }
-            else if(p[right]<=p[left]){
-                left=right;
+            else{
+                min = p[i];
             }
+            
 
         }
+        // int left =0;
+        // int right=1;
+        // int n=p.size();
+        // int maxi=0;
+        // for(;right<n;right++){
+        //     if(p[right]>p[left]){
+        //         maxi=max(maxi,p[right]-p[left]);
+
+        //     }
+        //     else if(p[right]<=p[left]){
+        //         left=right;
+        //     }
+
+        // }
         return maxi;
         
     }
