d01cf2070272c788439eb514bee5ce919ac50347
Sumer Singh <singhsumersingh35@gmail.com>
2026-07-11 18:33:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (99.49%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // sumer singh
-        int left =0;
-        int right =0;
-        string sub="";
-        int maxx=0;
-        for(;right<s.size();right++){
-            sub+=s[right];
-            while(count(sub.begin(),sub.end(),s[right])>1 && left<=right){
-                sub=sub.substr(1,sub.size());
-                left++;
-            }
-            maxx=max(maxx,right-left+1);
-            
+        int n = s.size();
+        int hashLength = 256;
+        int hash[hashLength];
+        for(int i = 0; i < hashLength; ++i){
+            hash[i] = -1;
         }
-        return maxx;
+        int right = 0, left = 0, maxLength = 0;
+        while(right < n){
+            if(hash[s[right]] != -1){
+                left = max(hash[s[right]] + 1, left);
+            }
+            int length = right - left + 1;
+            maxLength = max(length, maxLength);
+            hash[s[right]] = right;
+            right++;
+        }
 
-        
+        return maxLength;
     }
 };
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         // sumer singh
+//         int left =0;
+//         int right =0;
+//         string sub="";
+//         int maxx=0;
+//         for(;right<s.size();right++){
+//             sub+=s[right];
+//             while(count(sub.begin(),sub.end(),s[right])>1 && left<=right){
+//                 sub=sub.substr(1,sub.size());
+//                 left++;
+//             }
+//             maxx=max(maxx,right-left+1);
+            
+//         }
+//         return maxx;
+
+        
+//     }
+// };
