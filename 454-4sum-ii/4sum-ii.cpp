c3026ea5849a283098cda0eb72fc59d798b8e070
Sumer Singh <singhsumersingh35@gmail.com>
2026-07-11 14:41:18 +0530

    Time: 112 ms (31.44%) | Memory: 32.9 MB (5.35%) - LeetSync

---
+++ 454-4sum-ii/4sum-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {   
+        unordered_map<long long,int> h1;
+        unordered_map<long long,int> h2;
+        //sumer singh
+        int count=0;
+        int n = nums1.size();
+        for(int i=0;i<n;i++){
+          
+            for(int j=0;j<n;j++){
+                long long sum1=nums1[i];
+                long long sum2 = nums2[i];
+                sum1+=nums3[j];
+                sum2+=nums4[j];
+                
+                h1[sum1]++;
+                h2[sum2]++;
+            }
+           
+        }
+        for(auto ch : h1){
+            
+            long long s = -(ch.first);
+            if(h2.find(s)!=h2.end()){
+                count+=ch.second*h2[s];
+            }
+        }
+        
+      return count;  
+    }
+};
