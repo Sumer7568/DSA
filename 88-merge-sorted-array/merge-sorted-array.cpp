43b7b0c9176cf39b3815eac81143d9a325886463
Sumer Singh <singhsumersingh35@gmail.com>
2026-07-13 22:57:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (9.62%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -8,31 +8,52 @@ private:
     }
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int gap =(n+m)/2 + (n+m)%2;
-        
-        while(gap>0){
-            int left=0;
-            int right = left + gap;
 
-            while(right<n+m){
-                if(left<m && right>=m){
-                    swapifgreater(nums1,nums2,left,right-m);
-                }
-                else if (left>=m){
-                    swapifgreater(nums2,nums2,left-m,right-m);
-                }
-                else{
-                    swapifgreater(nums1,nums1,left,right);
-                }
-                right++,left++;
+        int p1 = n-1;
+        int p2 = m-1;
+        int p = m+n-1;
+
+        while(p1>=0){
+            if(p2>=0 && nums1[p2]>nums2[p1]){
+                nums1[p]=nums1[p2];
+                p2--;
             }
-            if(gap==1)break;
-            gap = gap/2 + gap%2;
+            else{
+                nums1[p]=nums2[p1];
+                p1--;
+            }
+            p--;
         }
+
+
+
+
+
+        // int gap =(n+m)/2 + (n+m)%2;
         
-        for(int i = m;i<n+m;i++){
-            nums1[i]=nums2[i-m];
-        }
+        // while(gap>0){
+        //     int left=0;
+        //     int right = left + gap;
+
+        //     while(right<n+m){
+        //         if(left<m && right>=m){
+        //             swapifgreater(nums1,nums2,left,right-m);
+        //         }
+        //         else if (left>=m){
+        //             swapifgreater(nums2,nums2,left-m,right-m);
+        //         }
+        //         else{
+        //             swapifgreater(nums1,nums1,left,right);
+        //         }
+        //         right++,left++;
+        //     }
+        //     if(gap==1)break;
+        //     gap = gap/2 + gap%2;
+        // }
+        
+        // for(int i = m;i<n+m;i++){
+        //     nums1[i]=nums2[i-m];
+        // }
         
     }
 };
