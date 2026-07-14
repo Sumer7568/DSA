5473878b837a016451049ce3e9bdce2d3cc003bf
Sumer Singh <singhsumersingh35@gmail.com>
2026-07-14 12:52:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (7.86%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int addDigits(int num){
+        if(num==0) return 0;
+        else if(num%9==0) return 9;
+        else return num%9;
+
+    }
+
+
+
+    // int addDigits(int num) {
+    //     if(num/10==0){
+    //         return num;
+    //     }
+
+    //     int sum =0;
+    //     while(num){
+    //         sum+=num%10;
+    //         num/=10;
+    //     }
+    //    return addDigits(sum);
+        
+
+
+    // }
+};
