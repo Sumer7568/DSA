7711f83fa382d66eb87ac0f02919fb62adf8b1a7
Sumer Singh <singhsumersingh35@gmail.com>
2026-08-13 20:17:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (47.66%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,29 +1,57 @@
 class Solution {
+
 public:
     string frequencySort(string s) {
-        int n = s.size();
-        vector<int> a(256,0);
-        for(int i=0;i<n;i++){
-            a[(int)s[i]]++;
+        
+        vector<int> freq(256, 0);
+        for(char c : s) {
+            freq[c]++;
         }
-        vector<string> b(n+1,"");
-        for(int i=0;i<256;i++){
-            b[a[i]]+=(char)i;
-        }
-        string ans="";
-        for(int i=n;i>=0;i--){
-            if(b[i]!=""){
-                for(char c : b[i]){
-                    string temp(i,c);
-                    ans+=temp;
-                }
+        
+
+        vector<pair<int, char>> v;
+        for(int i = 0; i < 256; i++) {
+            if(freq[i] > 0) {
+                v.push_back({freq[i], (char)i});
             }
         }
+   
+        sort(v.rbegin(), v.rend());
+        
+       
+        string ans = "";
+        for(auto p : v) {
+            ans.append(p.first, p.second);
+        }
+        
         return ans;
+    }
+};
+
+
+        // int n = s.size();
+        // vector<int> a(256,0);
+        // for(int i=0;i<n;i++){
+        //     a[(int)s[i]]++;
+        // }
+        // vector<string> b(n+1,"");
+        // for(int i=0;i<256;i++){
+        //     b[a[i]]+=(char)i;
+        // }
+        // string ans="";
+        // for(int i=n;i>=0;i--){
+        //     if(b[i]!=""){
+        //         for(char c : b[i]){
+        //             string temp(i,c);
+        //             ans+=temp;
+        //         }
+        //     }
+        // }
+        // return ans;
 
 
 
 
         
-    }
-};
+//     }
+// };
